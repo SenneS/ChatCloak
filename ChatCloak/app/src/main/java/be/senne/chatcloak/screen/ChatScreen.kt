0c81f084cd01343b33bf6e1c641d548f59e35df6
Senne <senne.de.neef@student.ehb.be>
2022-12-07 16:54:11 +0100

    chat screen view model test

--- ChatCloak/app/src/main/java/be/senne/chatcloak/screen/ChatScreen.kt
+++ ChatCloak/app/src/main/java/be/senne/chatcloak/screen/ChatScreen.kt
@@ -10,4 +10,11 @@ import be.senne.chatcloak.viewmodel.ChatVM
 @Composable
 fun createChatScreen(nav : NavController, key_container : KeyContainer, ip : String, is_host : Boolean, vm : ChatVM = viewModel()) {
     Text(text = "I'm the host? ${is_host} and I'm going to connect to ${ip}")
+    vm.myKey = key_container.myKeys
+    vm.theirKey = key_container.theirKey
+    vm.isHost = is_host
+
+    vm.generateSessionKey()
+    vm.setupConnection()
+
 }
--- ChatCloak/app/src/main/java/be/senne/chatcloak/viewmodel/ChatVM.kt
+++ ChatCloak/app/src/main/java/be/senne/chatcloak/viewmodel/ChatVM.kt
@@ -1,6 +1,56 @@
 package be.senne.chatcloak.viewmodel
 
 import androidx.lifecycle.ViewModel
+import java.io.DataInputStream
+import java.io.DataOutputStream
+import java.net.ServerSocket
+import java.net.Socket
+import java.security.KeyFactory
+import java.security.KeyPair
+import java.security.spec.X509EncodedKeySpec
+import javax.crypto.KeyAgreement
 
 class ChatVM : ViewModel() {
+    lateinit var myKey : KeyPair
+    lateinit var theirKey : String
+    lateinit var friendIp : String
+    lateinit var connectionSocket : Socket
+    lateinit var sessionKey : ByteArray
+    lateinit var sessionIv : ByteArray
+
+    var isHost : Boolean = false
+
+    lateinit var dataInputStream : DataInputStream
+    lateinit var dataOutputStream : DataOutputStream
+
+    fun setupConnection() {
+        if(isHost) {
+            val serverSocket = ServerSocket(21576)
+            connectionSocket = serverSocket.accept()
+
+            dataInputStream = DataInputStream(connectionSocket.getInputStream())
+            dataOutputStream = DataOutputStream(connectionSocket.getOutputStream())
+        }
+        else {
+            connectionSocket = Socket(friendIp, 21576)
+            dataInputStream = DataInputStream(connectionSocket.getInputStream())
+            dataOutputStream = DataOutputStream(connectionSocket.getOutputStream())
+        }
+    }
+
+    fun generateSessionKey() {
+        val keyAgreement = KeyAgreement.getInstance("ECDH", "BC")
+        keyAgreement.init(myKey.private)
+
+        val theirPublicKeyBytes = theirKey.encodeToByteArray()
+        val keyFactory = KeyFactory.getInstance("ECDH", "BC")
+        val theirPublicKey = keyFactory.generatePublic(X509EncodedKeySpec(theirPublicKeyBytes))
+
+        keyAgreement.doPhase(theirPublicKey, true)
+
+        val sharedSecret = keyAgreement.generateSecret()
+        sessionKey = sharedSecret.copyOfRange(0, 16)
+        sessionIv = sharedSecret.copyOfRange(16, 32)
+    }
+
 }
